ca735619d5801e9953dc10179c9c77ee15d66e60
heylarissa <larissah.dandrade@gmail.com>
2023-10-24 14:31:15 -0300

    space invaders

---
+++ .vscode/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FALSE 0
+
+int main () {
+    int end_game = FALSE;
+
+
+    while (!end_game) {
+
+
+        // atualiza tela
+        
+    }
+
+}
